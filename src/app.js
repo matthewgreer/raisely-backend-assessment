78b878cc7aa727e5cc66fcb9f4999debd4fedb0d
matthewgreer <matthew.greer.dev@gmail.com>
2024-05-10 17:50:59 -0400

    handle 404 for unknown routes

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ setupMiddleware(app);
 const routes = require('./api/routes');
 app.use('/', routes);
 
+// handle 404 errors for unknown routes
+app.use(function(req, res, next) {
+  res.status(404).json({ error: "Resource not found. Check the URL and try again." })
+});
+
 if (process.env.NODE_ENV !== 'TEST') {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
